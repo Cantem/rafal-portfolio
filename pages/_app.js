f8a31b09f2e1efdbe5344d9d2834b9d565879090
Rafal Porzucek <r.porzucek@gmail.com>
2022-03-21 10:35:46 +0000

    Pages improvements

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,28 @@
+import App from "next/app";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@/styles/index.scss";
+import Navbar from "@/components/shared/Navbar";
+import Hero from "@/components/shared/Hero";
+import Footer from "@/components/shared/Footer";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div className="portfolio-app">
+      <Navbar />
+      {pageProps.data}
+      {Component.name === "Home" && <Hero />}
+      <div className="container">
+        <Component {...pageProps} />
+      </div>
+      <Footer />
+    </div>
+  );
 }
+
+MyApp.getInitialProps = async (context) => {
+  const initialProps =
+    App.getInitialProps && (await App.getInitialProps(context));
+  console.log("initialProps", initialProps);
+  return { pageProps: { data: "app data", ...initialProps.pageProps } };
+};
 export default MyApp;
