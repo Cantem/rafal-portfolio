3e66785f22d4e0acc0e678712da08267b32024ed
Rafal Porzucek <r.porzucek@gmail.com>
2022-05-20 12:55:55 +0100

    Refactor get portfolios

--- pages/portfolios/[id].js
+++ pages/portfolios/[id].js
@@ -1,17 +1,21 @@
-import React from "react";
-import { useQuery } from "@apollo/react-hooks";
+import React, { useState, useEffect } from "react";
+import { useQuery, useLazyQuery } from "@apollo/react-hooks";
 import { GET_PORTFOLIO } from "@/apollo/queries";
 
 const PortfolioDetail = ({ query }) => {
-  const { loading, error, data } = useQuery(GET_PORTFOLIO, {
-    variables: { id: query.id },
-  });
+  const [portfolio, setPortfolio] = useState(null);
+  const [getPortfolio, { loading, data }] = useLazyQuery(GET_PORTFOLIO);
 
-  if (loading) {
+  useEffect(() => {
+    getPortfolio({ variables: { id: query.id } });
+  }, []);
+
+  if (data && !portfolio) {
+    setPortfolio(data.portfolio);
+  }
+  if (loading || !portfolio) {
     return "Loading...";
   }
-
-  const portfolio = (data && data.portfolio) || {};
 
   return (
     <div className="portfolio-detail">
