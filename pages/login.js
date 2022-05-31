31079c1a2060f074ea47b68ad76dcf27541ad0e9
Rafal Porzucek <r.porzucek@gmail.com>
2022-05-31 14:49:01 +0100

    sign in success

--- pages/login.js
+++ pages/login.js
@@ -1,15 +1,39 @@
 import LoginForm from "components/forms/LoginForm";
+import withApollo from "hoc/withApollo";
+import { Mutation } from "@apollo/react-components";
+import { SIGN_IN } from "apollo/queries/index.js";
+import Redirect from "components/shared/Redirect.js";
 
 const Login = () => {
+  const errorMessage = (error) => {
+    return error.message || "Ooooops something went wrong...";
+  };
+
   return (
     <>
       <div className="bwm-form mt-5">
         <div className="row">
           <div className="col-md-5 mx-auto">
             <h1 className="page-title">Login</h1>
-            <LoginForm
-              onSubmit={(signInData) => alert(JSON.stringify(signInData))}
-            />
+            <Mutation
+              mutation={SIGN_IN}
+              // TODO fix workaround for undhandled error thrown in the UI
+              onError={() => {}}
+            >
+              {(signIn, { data, error }) => (
+                <>
+                  <LoginForm
+                    onSubmit={(signInData) => signIn({ variables: signInData })}
+                  />
+                  {data && data.signIn && <Redirect to="/" />}
+                  {error && (
+                    <div className="alert alert-danger">
+                      {errorMessage(error)}
+                    </div>
+                  )}
+                </>
+              )}
+            </Mutation>
           </div>
         </div>
       </div>
@@ -17,4 +41,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default withApollo(Login);
